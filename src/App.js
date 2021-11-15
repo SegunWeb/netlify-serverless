6834d25fe419a97cd410c015559a845cd1cb0bb0
SegunWeb <mrsegun2014@gmail.com>
2021-11-15 22:24:17 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './style.css'
+import axios from "axios";
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [result, setResult] = useState(null)
+    const fetchData = async () => {
+        const results = await axios.get('/.netlify/functions/helloWorld')
+        console.log(results)
+        setResult(results.data.message)
+    }
+
+    return (
+        <div className="app">
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo"/>
+                <h1 className='title'>Hi Serverless!!!</h1>
+            </header>
+            <div className='main'>
+                <p className='text-message'><span>message:</span> {result}</p>
+                <button className='btn' onClick={fetchData}>Get data</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
